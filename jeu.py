a4af9ddfbaa10d5a102ad080420753dfb69601d1
H4CK3R5-FS <h4ck3r5pc@gmail.com>
2022-05-18 17:23:57 +0200

    push

--- jeu.py
+++ jeu.py
@@ -62,7 +62,7 @@ class Game :
 
     def switch_house(self):
          #chargement carte
-        tmx_data = pytmx.util_pygame.load_pygame('house.tmx')
+        tmx_data = pytmx.util_pygame.load_pygame('./house.tmx')
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
         map_layer.zoom = 2
